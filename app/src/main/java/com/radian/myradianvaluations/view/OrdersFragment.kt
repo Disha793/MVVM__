1316f71aa82bb0baa912d7b70b711df9baba4203
Nidhi Desai <nidhi.desai@synoverge.com>
2021-03-01 11:45:17 +0530

    Id: 2424 Bottom/navigation Orders tab

--- app/src/main/java/com/radian/myradianvaluations/view/OrdersFragment.kt
+++ app/src/main/java/com/radian/myradianvaluations/view/OrdersFragment.kt
@@ -8,28 +8,24 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.radian.myradianvaluations.R
 import com.radian.myradianvaluations.adapter.OrdersAdapter
 import com.radian.myradianvaluations.constants.APIStatus
 import com.radian.myradianvaluations.interfaces.OpenOrderClickListener
-import com.radian.myradianvaluations.network.APIList
-import com.radian.myradianvaluations.network.RetrofitBase
 import com.radian.myradianvaluations.utils.CommonUtils
 import com.radian.myradianvaluations.utils.LoadingDialog
-import com.radian.myradianvaluations.utils.LogUtils
-import com.radian.myradianvaluations.utils.Pref
+import com.radian.myradianvaluations.viewmodel.OrdersViewModel
 import com.radian.vendorbridge.Response.OrderListResponse
-import io.reactivex.Observer
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.activity_bottom_navigation.*
 import kotlinx.android.synthetic.main.fragment_orders.view.*
 import kotlinx.android.synthetic.main.layout_toolbar.*
 
-class OrdersFragment : Fragment(){
+class OrdersFragment : Fragment() {
     internal lateinit var view: View
     private val classTag = this::class.java.canonicalName!!
     private val orderList = ArrayList<OrderListResponse.Data.OrderDetail>()
+    private lateinit var orderViewModel: OrdersViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -38,14 +34,16 @@ class OrdersFragment : Fragment(){
     ): View? {
         view = inflater.inflate(R.layout.fragment_orders, container, false)
         (context as BottomNavigationActivity).bottomNavigationView.visibility = View.VISIBLE
-
+        orderViewModel =
+            ViewModelProvider(context as BottomNavigationActivity).get(OrdersViewModel::class.java)
+        orderViewModel.init(context as BottomNavigationActivity)
         view.recyclerView.adapter = OrdersAdapter(context!!, object : OpenOrderClickListener {
             override fun onOpenOrderClick(position: Int) {
-//                (context as BottomNavigationActivity).pushFragment(
-//                    ManageOrderFragment.newInstance(
-//                        orderList.get(position).itemId!!
-//                    ), true
-//                )
+                (context as BottomNavigationActivity).pushFragment(
+                    ManageOrderFragment.newInstance(
+                        orderList.get(position).itemId!!
+                    ), true
+                )
             }
 
         }, orderList)
@@ -64,66 +62,44 @@ class OrdersFragment : Fragment(){
     }
 
     private fun getOrderList() {
-        RetrofitBase.getClient().create(APIList::class.java).orderList(
-            Pref.getValue(context!!, Pref.AUTH_TOKEN, "")!!,
-            Pref.getValue(context!!, Pref.PHONE_NUMBER, "")!!,
-            CommonUtils.getDeviceUUID(context!!),
-            Pref.getValue(context!!, Pref.MOBILE_USER_ID, 0)!!,
-            Pref.getValue(context!!, Pref.ORGANIZATN_ID, 0)!!
-        ).subscribeOn(io.reactivex.schedulers.Schedulers.io())
-
-            .observeOn(AndroidSchedulers.mainThread())
-
-            .subscribe(object : Observer<OrderListResponse> {
-                override fun onComplete() {
-                    LoadingDialog.dismissDialog()
-                }
-
-                override fun onSubscribe(d: Disposable) {
-                    LoadingDialog.show(context!!)
-                }
-
-                override fun onNext(t: OrderListResponse) {
-                    if (t.status == APIStatus.ok) {
-                        orderList.clear()
-                        orderList.addAll(t.data.orderDetails)
-                        view.recyclerView.adapter?.notifyDataSetChanged()
-                        if(orderList.isEmpty())
-                        {
-                            view.txtNoOpenOrdr.visibility= View.VISIBLE
-                        }
-                        else
-                        {
-                            view.txtNoOpenOrdr.visibility= View.GONE
-                        }
-                    } else if (t.status.equals(APIStatus.unauth, true)) {
-                        Toast.makeText(
-                            context!!,
-                            t.errorInfo.get(0).errorMessage,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                        var intent = Intent(context!!, PasscodeActivity::class.java)
-                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
-                        startActivity(intent)
+        orderViewModel.getOrderList().let {
+            LoadingDialog.show(context!!)
+            it?.observe(context as BottomNavigationActivity, androidx.lifecycle.Observer {
+                LoadingDialog.dismissDialog()
+                if (it.status == APIStatus.ok) {
+                    orderList.clear()
+                    orderList.addAll(it.data.orderDetails)
+                    view.recyclerView.adapter?.notifyDataSetChanged()
+                    if (orderList.isEmpty()) {
+                        view.txtNoOpenOrdr.visibility = View.VISIBLE
+                    } else {
+                        view.txtNoOpenOrdr.visibility = View.GONE
                     }
-
-                }
-
-                override fun onError(e: Throwable) {
-
-                    LogUtils.logE(classTag, e.message!!, e)
-                    LoadingDialog.dismissDialog()
-                    CommonUtils.showOkDialog(
+                } else if (it.status.equals(APIStatus.unauth, true)) {
+                    Toast.makeText(
                         context!!,
-                        getString(R.string.please_try_again),
-                        DialogInterface.OnClickListener { _, _ ->
-
-                        },
-                        getString(R.string.ok)
-                    )
+                        it.errorInfo.get(0).errorMessage,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    var intent = Intent(context!!, PasscodeActivity::class.java)
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+                    startActivity(intent)
                 }
-
             })
+            if (it == null) {
+                LoadingDialog.dismissDialog()
+                CommonUtils.showOkDialog(
+                    context!!,
+                    getString(R.string.please_try_again),
+                    DialogInterface.OnClickListener { _, _ ->
+
+                    },
+                    getString(R.string.ok)
+                )
+
+            }
+        }
+
 
     }
 }
--- app/src/main/java/com/radian/myradianvaluations/viewmodel/OrdersViewModel.kt
+++ app/src/main/java/com/radian/myradianvaluations/viewmodel/OrdersViewModel.kt
@@ -1,2 +1,19 @@
 package com.radian.myradianvaluations.viewmodel
 
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.radian.myradianvaluations.repository.DashboardRepository
+import com.radian.myradianvaluations.repository.OrdersRepository
+import com.radian.vendorbridge.Response.OrderListResponse
+
+class OrdersViewModel :ViewModel(){
+    private lateinit var orderRepository: OrdersRepository
+    fun init(context: Context) {
+        orderRepository = OrdersRepository.getInstance(context)
+    }
+    fun getOrderList():MutableLiveData<OrderListResponse>?{
+        return orderRepository.getOrderList()
+    }
+
+}
